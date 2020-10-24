2b7f71f243acd99a3a3652c2b18babd62f6c0a77
Kevin Perry <perry@fhwn.ac.at>
2020-10-24 12:41:31 +0200

    Reformat according to editorconfig

--- TrailingZeros.Test/TrailingZeros.Test.cs
+++ TrailingZeros.Test/TrailingZeros.Test.cs
@@ -5,37 +5,37 @@ using NUnit.Framework;
 
 namespace TrailingZeros.Test
 {
-    public class Tests
+  public class Tests
+  {
+    private static IEnumerable TrailingZerosData
     {
-        private static IEnumerable TrailingZerosData
-        {
-            get
-            {
-                yield return new TestCaseData(new BigInteger(0)).Returns(0);
-                yield return new TestCaseData(new BigInteger(1)).Returns(0);
-                yield return new TestCaseData(new BigInteger(10)).Returns(1);
-                yield return new TestCaseData(new BigInteger(90)).Returns(1);
-                yield return new TestCaseData(new BigInteger(99)).Returns(0);
-                yield return new TestCaseData(new BigInteger(100)).Returns(2);
-                yield return new TestCaseData(new BigInteger(4711000)).Returns(3);
-                yield return new TestCaseData(new BigInteger(4711001)).Returns(0);
-                yield return new TestCaseData(BigInteger.Parse("12345678900000000000000000000")).Returns(20);
-                yield return new TestCaseData(BigInteger.Parse("12345678900000000000000000001")).Returns(0);
+      get
+      {
+        yield return new TestCaseData(new BigInteger(0)).Returns(0);
+        yield return new TestCaseData(new BigInteger(1)).Returns(0);
+        yield return new TestCaseData(new BigInteger(10)).Returns(1);
+        yield return new TestCaseData(new BigInteger(90)).Returns(1);
+        yield return new TestCaseData(new BigInteger(99)).Returns(0);
+        yield return new TestCaseData(new BigInteger(100)).Returns(2);
+        yield return new TestCaseData(new BigInteger(4711000)).Returns(3);
+        yield return new TestCaseData(new BigInteger(4711001)).Returns(0);
+        yield return new TestCaseData(BigInteger.Parse("12345678900000000000000000000")).Returns(20);
+        yield return new TestCaseData(BigInteger.Parse("12345678900000000000000000001")).Returns(0);
 
-                yield return new TestCaseData(new BigInteger(-0)).Returns(0);
-                yield return new TestCaseData(new BigInteger(-1)).Returns(0);
-                yield return new TestCaseData(new BigInteger(-10)).Returns(1);
-                yield return new TestCaseData(new BigInteger(-90)).Returns(1);
-                yield return new TestCaseData(new BigInteger(-99)).Returns(0);
-                yield return new TestCaseData(new BigInteger(-100)).Returns(2);
-                yield return new TestCaseData(new BigInteger(-4711000)).Returns(3);
-                yield return new TestCaseData(new BigInteger(-4711001)).Returns(0);
-                yield return new TestCaseData(BigInteger.Parse("-12345678900000000000000000000")).Returns(20);
-                yield return new TestCaseData(BigInteger.Parse("-12345678900000000000000000001")).Returns(0);
-            }
-        }
-
-        [TestCaseSource("TrailingZerosData")]
-        public int TrailingZerosAreCountedCorrectly(BigInteger value) => value.TrailingZeroCount();
+        yield return new TestCaseData(new BigInteger(-0)).Returns(0);
+        yield return new TestCaseData(new BigInteger(-1)).Returns(0);
+        yield return new TestCaseData(new BigInteger(-10)).Returns(1);
+        yield return new TestCaseData(new BigInteger(-90)).Returns(1);
+        yield return new TestCaseData(new BigInteger(-99)).Returns(0);
+        yield return new TestCaseData(new BigInteger(-100)).Returns(2);
+        yield return new TestCaseData(new BigInteger(-4711000)).Returns(3);
+        yield return new TestCaseData(new BigInteger(-4711001)).Returns(0);
+        yield return new TestCaseData(BigInteger.Parse("-12345678900000000000000000000")).Returns(20);
+        yield return new TestCaseData(BigInteger.Parse("-12345678900000000000000000001")).Returns(0);
+      }
     }
+
+    [TestCaseSource("TrailingZerosData")]
+    public int TrailingZerosAreCountedCorrectly(BigInteger value) => value.TrailingZeroCount();
+  }
 }
--- TrailingZeros/TrailingZeros.cs
+++ TrailingZeros/TrailingZeros.cs
@@ -2,17 +2,17 @@
 
 namespace TrailingZeros
 {
-    public static class TrailingZeroCounter
+  public static class TrailingZeroCounter
+  {
+    public static int TrailingZeroCount(this BigInteger value)
     {
-        public static int TrailingZeroCount(this BigInteger value)
-        {
-            if (value.IsZero)
-                return 0;
+      if (value.IsZero)
+        return 0;
 
-            var count = 0;
-            for (; (value % 10).IsZero; value /= 10, ++count) ;
+      var count = 0;
+      for (; (value % 10).IsZero; value /= 10, ++count) ;
 
-            return count;
-        }
+      return count;
     }
+  }
 }
