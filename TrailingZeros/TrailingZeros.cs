6fced6083760a8965ce5340b815756b0f2c8791c
Kevin Perry <perry@fhwn.ac.at>
2020-10-24 12:27:45 +0200

    Initial commit

---
+++ TrailingZeros/TrailingZeros.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+
+namespace TrailingZeros
+{
+    public static class TrailingZeroCounter
+    {
+        public static int TrailingZeroCount(this BigInteger value)
+        {
+            if (value == 0)
+                return 0;
+
+            var count = 0;
+            for (; value % 10 == 0; value /= 10, ++count) ;
+
+            return count;
+        }
+    }
+}
